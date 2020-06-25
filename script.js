ab0038d77ba83b131468450dd21d82c695f60b51
Dr. Sockies <51517812+whitesockies@users.noreply.github.com>
2020-06-25 18:59:14 -0500

    uwu

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function headerText(){
+document.getElementById("header").addEventListener("mouseover", mouseOver);
+document.getElementById("header").addEventListener("mouseout", mouseOut);
+function mouseOver(){
 	document.getElementById("header").innerHTML = "\ sockies \";
+	document.getElementById("header").style.color = "red";
+}
+function mouseOut(){
+	document.getElementById("header").innerHTML = "/ sockies /";
+	document.getElementById("header").innerHTML = "green";
 }
