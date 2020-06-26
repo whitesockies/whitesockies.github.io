34d8b17c222e2901459580bca5010efcc1958674
Dr. Sockies <51517812+whitesockies@users.noreply.github.com>
2020-06-25 19:16:30 -0500

    LOL! i give up

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.getElementById("header").addEventListener("mouseover", mouseOver);
-document.getElementById("header").addEventListener("mouseout", mouseOut);
-function mouseOver(){
-	document.getElementById("header").innerHTML = "\ sockies \";
-	document.getElementById("header").style.color = "red";
-}
-function mouseOut(){
-	document.getElementById("header").innerHTML = "/ sockies /";
-	document.getElementById("header").innerHTML = "green";
-}
+// lol
